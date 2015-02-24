d3de69cb24cbe964042e3a1108ea075703d4965b
nicklee76 <coolnicklee@hotmail.com>
2015-02-24 15:06:19 -0800

    edit comments

--- softlayer_sandbox.py
+++ softlayer_sandbox.py
@@ -1,16 +1,27 @@
 #!/usr/bin/env python
 
-# coolnick
-# requires following in order for this script to work
-#   + a configuration file called "softlayer_config.json" file under the same directory
-#   + a post provision script file accessible via internet
+# Description: Script to spin up virtual servers in SoftLayer environment.
+#
+# Requirements: Script requires following in order for this script to work
+#   Packages:       SoftLayer
+#   File(s):        Configuration file called "softlayer_config.json" file under the same directory and
+#                   a post provision script file that is accessible via internet
+#
+# Command line / Terminal arguments: The script would take 3 arguments from the terminal / command line.
+#   --unique_id     Optional(default = epoch time).  When virtual servers are created using the script, it will
+#                   include a prefix.  It will use  --unique_id value as the prefix and if it is not given, it will
+#                   use epoch time by default.
+#   --debug         Optional (default = False).  Enable debug mode for more information on what is happening.
+#   --config_file   Optional (default = './softlayer_config.json').  Location & name of the file(JSON) that
+#                   contains the configs.  If not given, it will assume a config file called 'softlayer_config.json'
+#                   file is located under the same directory by default.
 
 import sys, time, os, json, argparse
 
 try:
     import SoftLayer
 except ImportError:
-    print('\n[CoOlNiCk] You need [SoftLayer] modules to run this script')
+    print('\n[Import Error] You need [SoftLayer] package to run this script')
     sys.exit('Terminating the process...')
 
 parser = argparse.ArgumentParser()
